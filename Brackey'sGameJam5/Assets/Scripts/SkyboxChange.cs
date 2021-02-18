76ec49e325ee570299e068b400f3d95421797108
Alex <Smits>
2021-02-18 14:40:07 +1100

    Tweaked all audio / animation triggers

--- Brackey'sGameJam5/Assets/Scripts/SkyboxChange.cs
+++ Brackey'sGameJam5/Assets/Scripts/SkyboxChange.cs
@@ -1,24 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SkyboxChange : MonoBehaviour
 {
     public Material skybox1;
     public Material skybox2;
 
-
-    private void Start()
-    {
-        RenderSettings.skybox = skybox1;
-    }
     private void OnTriggerEnter(Collider target)
     {
         if (target.tag == "Player")
             RenderSettings.skybox = skybox2;
     }
 
-    // Update is called once per frame
     private void OnTriggerExit(Collider target)
     {
         if (target.tag == "Player")
