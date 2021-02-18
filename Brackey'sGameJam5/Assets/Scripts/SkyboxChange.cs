6da91efb10572a8f8e1c17c4b5234a3f3c68f16c
Alex <Smits>
2021-02-18 16:30:49 +1100

    Start of Intro

--- Brackey'sGameJam5/Assets/Scripts/SkyboxChange.cs
+++ Brackey'sGameJam5/Assets/Scripts/SkyboxChange.cs
@@ -4,7 +4,7 @@ public class SkyboxChange : MonoBehaviour
 {
     public Material skybox1;
     public Material skybox2;
-
+ 
     private void OnTriggerEnter(Collider target)
     {
         if (target.tag == "Player")
